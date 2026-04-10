6da94df4c5eebf326a8ae494c446c10e91e2dbeb
Manolis Zacha <manoliszaxa@gmail.com>
2026-04-10 10:40:44 +0300

    add a fucntionlity of hit in cache 1

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 set -e # if any of below commands is failed then the script will be terminate, if i didnt use this command then if any command is failed then the script will continue
 
-# Install Databricks CLI
-curl -fsSL https://raw.githubusercontent.com/databricks/setup-cli/main/install.sh | sh
-
 # Print Databricks CLI version
 databricks -v
 
@@ -21,7 +18,3 @@ cat ~/.databrickscfg
 
 # Verify auth profiles
 databricks auth profiles
-
-# Install dependencies
-pip install uv
-uv sync
